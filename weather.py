6c0b85ad6115bc176f69045495cd9b3736d82db5
uniqname <uniqname@umich.edu>
2023-04-21 16:28:17 -0400

    changed color

--- weather.py
+++ weather.py
@@ -100,7 +100,7 @@ def calculate_pollen_relations(lat, longi):
 
 def create_bar_graph(cities, list_of_items):
     fig, ax = plt.subplots()
-    ax.bar(cities, np.ravel(list_of_items))
+    ax.bar(cities, np.ravel(list_of_items), color = 'green')
 
     ax.set_title('Temperature and Relative Humidity Relations by City')
     ax.set_xlabel('City')
@@ -111,7 +111,7 @@ def create_bar_graph(cities, list_of_items):
 def main(): 
     ann_arbor_weather = get_weather_info("latitude=42.28&longitude=-83.74&hourly=temperature_2m,relativehumidity_2m,visibility,windspeed_120m&temperature_unit=fahrenheit&windspeed_unit=mph&forecast_days=1&start_date=2023-04-19&end_date=2023-04-19&timezone=America%2FNew_York")
     tustin_weather = get_weather_info("latitude=33.75&longitude=-117.83&hourly=temperature_2m,relativehumidity_2m,visibility,windspeed_120m&temperature_unit=fahrenheit&windspeed_unit=mph&forecast_days=1&start_date=2023-04-19&end_date=2023-04-19&timezone=America%2FNew_York")
-    seattle_weather = get_weather_info("latitude=47.61&longitude=-122.33&hourly=temperature_2m,relativehumidity_2m,visibility,windspeed_120m&temperature_unit=fahrenheit&windspeed_unit=mph&forecast_days=1&start_date=2023-04-19&end_date=2023-04-19&timezone=America%2FNew_York")
+    shanghai_weather = get_weather_info("latitude=31.22&longitude=121.46&hourly=temperature_2m,relativehumidity_2m,visibility,windspeed_120m&temperature_unit=fahrenheit&windspeed_unit=mph&forecast_days=1&start_date=2023-04-19&end_date=2023-04-19&timezone=America%2FNew_York")
     tokyo_weather = get_weather_info("latitude=35.69&longitude=139.69&hourly=temperature_2m,relativehumidity_2m,visibility,windspeed_120m&temperature_unit=fahrenheit&windspeed_unit=mph&forecast_days=1&start_date=2023-04-19&end_date=2023-04-19&timezone=America%2FNew_York")
     sydney_weather = get_weather_info("latitude=-33.87&longitude=151.21&hourly=temperature_2m,relativehumidity_2m,visibility,windspeed_120m&temperature_unit=fahrenheit&windspeed_unit=mph&forecast_days=1&start_date=2023-04-19&end_date=2023-04-19&timezone=America%2FNew_York")
     new_york_weather = get_weather_info("latitude=40.71&longitude=-74.01&hourly=temperature_2m,relativehumidity_2m,visibility,windspeed_120m&temperature_unit=fahrenheit&windspeed_unit=mph&forecast_days=1&start_date=2023-04-19&end_date=2023-04-19&timezone=America%2FNew_York")
@@ -125,7 +125,7 @@ def main():
 
     database_processing(ann_arbor_weather, aqdb)
     database_processing(tustin_weather, aqdb)
-    database_processing(seattle_weather,aqdb)
+    database_processing(shanghai_weather,aqdb)
     database_processing(tokyo_weather, aqdb)
     database_processing(sydney_weather, aqdb)
     database_processing(new_york_weather, aqdb)
@@ -134,7 +134,7 @@ def main():
 
     # database_processing(ann_arbor_weather, omdb)
     # database_processing(tustin_weather, omdb)
-    # database_processing(seattle_weather,omdb)
+    # database_processing(shanghai_weather,omdb)
     # database_processing(tokyo_weather, omdb)
     # database_processing(sydney_weather, omdb)
     # database_processing(new_york_weather, omdb)
@@ -143,7 +143,7 @@ def main():
     pollen_relations_list = []
     aa_pollen = calculate_pollen_relations(42.292328, -83.736755)
     tustin_pollen = calculate_pollen_relations(33.752544, -117.81802)
-    seattle_pollen = calculate_pollen_relations(47.621212, -122.33498)
+    shanghai_pollen = calculate_pollen_relations(31.2, 121.4375)
     tokyo_pollen = calculate_pollen_relations(35.7, 139.6875)
     sydney_pollen = calculate_pollen_relations(-33.75, 151.125)
     ny_pollen = calculate_pollen_relations(40.710335, -73.99307)
@@ -151,13 +151,13 @@ def main():
 
     pollen_relations_list.append(aa_pollen)
     pollen_relations_list.append(tustin_pollen)
-    pollen_relations_list.append(seattle_pollen)
+    pollen_relations_list.append(shanghai_pollen)
     pollen_relations_list.append(tokyo_pollen)
     pollen_relations_list.append(sydney_pollen)
     pollen_relations_list.append(ny_pollen)
     pollen_relations_list.append(london_pollen)
 
-    cities = ['Ann Arbor, MI', 'Tustin, CA', 'Seattle, WA', 'Tokyo, Japan', 'Sydney, Australia', 'New York, NY', 'London, UK']
+    cities = ['Ann Arbor, MI', 'Tustin, CA', 'Shanghai, China', 'Tokyo, Japan', 'Sydney, Australia', 'New York, NY', 'London, UK']
     create_bar_graph(cities, pollen_relations_list)
     
 
